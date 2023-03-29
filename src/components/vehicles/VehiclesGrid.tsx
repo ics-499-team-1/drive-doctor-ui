b9ff97bd508047a87138eb1074e6affc395508ce
Vontha Chan <vontha.chan@my.metrostate.edu>
2023-03-28 21:13:52 -0500

    Fixed layout

--- src/components/vehicles/VehiclesGrid.tsx
+++ src/components/vehicles/VehiclesGrid.tsx
@@ -1,23 +1,21 @@
 import { Button, Container, SimpleGrid } from "@chakra-ui/react";
 import VehiclesCard from "./VehiclesCard";
-import "./VehiclesGrid.css"
+import "./VehiclesGrid.css";
 
 function VehiclesGrid() {
   return (
     <>
-      <SimpleGrid columns={2} spacing={10} margin="10px">
-        <VehiclesCard information="Vehicle 1" />
-        <VehiclesCard information="Vehicle 2" />
-        <VehiclesCard information="Vehicle 3" />
-        <VehiclesCard information="Vehicle 4" />
-        <VehiclesCard information="Vehicle 5" />
-        <VehiclesCard information="Vehicle 6" />
-        <VehiclesCard information="Vehicle 7" />
+      <SimpleGrid columns={1}>
+        <SimpleGrid columns={2} spacing={10} margin="10px">
+          <VehiclesCard information="Vehicle 1" />
+          <VehiclesCard information="Vehicle 2" />
+          <VehiclesCard information="Vehicle 3" />
+        </SimpleGrid>
+        <SimpleGrid columns={2} spacing={10} margin="10px">
+          <Button className="editBtn">Edit</Button>
+          <Button className="addBtn">Add</Button>
+        </SimpleGrid>
       </SimpleGrid>
-      <Container className="btnContainer">
-      <Button className="editBtn">Edit</Button>
-      <Button className="addBtn">Add</Button>
-      </Container>
     </>
   );
 }
